36b095fb91bbc7d36fab74bc14067f769f47e13d
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2025-09-26 00:52:19 -0500

    update controller and service to new login

---
+++ Vertical360_back/Application/Contracts/Auth/LoginResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Vertical360_back.Application.Contracts.Auth
+{
+    public class LoginResultDto
+    {
+        public bool Success { get; set; }
+        public string? UserId { get; set; }
+        public string? RedirectUrl { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
---
+++ Vertical360_back/Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Vertical360_back.Application.Contracts.Auth;
+
+namespace Vertical360_back.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResultDto> LoginAsync(string email, string password, bool rememberMe);
+    }
+}
