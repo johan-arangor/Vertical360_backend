178904c1d906a80ed4e7f613dfb19002806222bc
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2025-10-02 00:19:16 -0500

    crear el baseentity para usar en todas las entitades

---
+++ Vertical360_back/Domain/Entityes/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vertical360_back.Domain.Entityes
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
