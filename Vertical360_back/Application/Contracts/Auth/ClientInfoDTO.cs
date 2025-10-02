aad9a979d7ac8cb1a9a8db1f3a041acdcd2c9589
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2025-10-02 00:20:45 -0500

    agregar contracts

---
+++ Vertical360_back/Application/Contracts/Auth/ClientInfoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Vertical360_back.Application.Contracts.Auth
+{
+    public class ClientInfoDTO
+    {
+        public Guid CompanyId { get; set; }
+        public string Name { get; set; } = null!;
+        public string ConnectionString { get; set; } = null!;
+    }
+}
