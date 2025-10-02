ae9c1882ef1a7150272df7eaa909716f266ffe11
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2025-10-02 00:17:43 -0500

    editar y agregar los nuevos DTO para la aplicacion

---
+++ Vertical360_back/Application/Contracts/DTOs/CitySeedDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Vertical360_back.Application.Contracts.DTOs
+{
+    public class CitySeedDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!;
+        public string DepartmentCode { get; set; } = null!;
+    }
+}
---
+++ Vertical360_back/Application/Contracts/DTOs/DepartmentSeedDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Vertical360_back.Application.Contracts.DTOs
+{
+    public class DepartmentSeedDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!;
+        public string CountryCode { get; set; } = null!;
+    }
+}
