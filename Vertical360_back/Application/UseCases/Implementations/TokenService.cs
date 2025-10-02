7d5066b47572328fff789ca88efc3253e5ca8fab
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2025-10-02 00:18:22 -0500

    crear la logica para casos de uso

---
+++ Vertical360_back/Application/UseCases/Implementations/TokenService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Vertical360_back.Application.Configuration;
+using Vertical360_back.Application.Interfaces.Services;
+
+namespace Vertical360_back.Application.UseCases.Implementations
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TokenService(IOptions<JwtSettings> jwtSettings, UserManager<IdentityUser> userManager)
+        {
+            _jwtSettings = jwtSettings.Value;
+            _userManager = userManager;
+        }
+
+        public async Task<string> GenerateTokenAsync(IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName ?? user.Email),
+                new Claim(JwtRegisteredClaimNames.Iss, _jwtSettings.Issuer),
+                new Claim(JwtRegisteredClaimNames.Aud, _jwtSettings.Audience)
+            };
+
+            // Agregar roles al token
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            // Clave de seguridad (obtenida de la configuración)
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Creación del token
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.DurationInMinutes),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
