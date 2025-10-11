d6b1f1aa600a27353e8c32d5d88bf4a99ea99510
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2025-10-11 18:37:44 -0500

    delete files not used in process of create companies

--- Vertical360_back/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Vertical360_back.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Vertical360_back/Models/LinkUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vertical360_back.Models
-{
-    public class LinkUser
-    {
-        public Guid CompanyId { get; set; }
-        public string NameCompany { get; set; } = null!;
-        public string EmailUser { get; set; } = null!;
-    }
-}
