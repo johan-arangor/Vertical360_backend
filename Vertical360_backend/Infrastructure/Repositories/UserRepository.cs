a49a6680c6ed310cff0601ff9ccadede260782c5
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2026-03-16 23:46:32 -0500

    update repositories infrstructure data

--- Vertical360_backend/Infrastructure/Repositories/UserRepository.cs
+++ Vertical360_backend/Infrastructure/Repositories/UserRepository.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Vertical360_backend.Application.Interfaces;
 using Vertical360_backend.Domain.Entities;
@@ -9,9 +9,9 @@ namespace Vertical360_backend.Infrastructure.Repositories
     public class UserRepository : IUserRepository
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ApplicationDbContext _context;
+        private readonly MasterDbContext _context;
 
-        public UserRepository(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        public UserRepository(UserManager<ApplicationUser> userManager, MasterDbContext context)
         {
             _userManager = userManager;
             _context = context;
@@ -21,13 +21,16 @@ namespace Vertical360_backend.Infrastructure.Repositories
             => await _userManager.FindByIdAsync(id);
 
         public async Task<IEnumerable<ApplicationUser>> GetAllAsync(string tenantId)
-            => await _context.Users.Where(u => u.TenantId == tenantId || u.TenantId == null).ToListAsync();
+            => await _context.Users
+                .Where(u => u.TenantId == tenantId || u.TenantId == null)
+                .ToListAsync();
 
         public async Task CreateAsync(ApplicationUser user, string password, string role, string tenantId)
         {
             user.TenantId = tenantId;
             var res = await _userManager.CreateAsync(user, password);
-            if (!res.Succeeded) throw new Exception(string.Join("; ", res.Errors.Select(e => e.Description)));
+            if (!res.Succeeded)
+                throw new Exception(string.Join("; ", res.Errors.Select(e => e.Description)));
             await _userManager.AddToRoleAsync(user, role);
         }
     }
