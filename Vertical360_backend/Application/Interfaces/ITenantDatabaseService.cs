a7def5ec000b4490b51dc38b8b5ae246aa743c0f
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2026-03-16 23:47:35 -0500

    update an add new interface to tenant service and fatory

--- Vertical360_backend/Application/Interfaces/ITenantDatabaseService.cs
+++ Vertical360_backend/Application/Interfaces/ITenantDatabaseService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Vertical360_backend.Application.Interfaces
 {
@@ -6,5 +6,11 @@ namespace Vertical360_backend.Application.Interfaces
     {
         Task<DbContext> GetTenantDbContextAsync(string tenantId);
         Task CreateTenantDatabaseAsync(string tenantId);
+
+        /// <summary>
+        /// Elimina la base de datos del tenant. Se usa como rollback
+        /// si la creación de la compañía falla después de haber creado la BD.
+        /// </summary>
+        Task DropTenantDatabaseAsync(string tenantId);
     }
 }
