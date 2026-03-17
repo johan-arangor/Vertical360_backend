7c30411d27eaac4075d97c90717507d624a51fb8
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2026-03-16 20:23:17 -0500

    update autht DTO for authorize client

--- Vertical360_backend/Application/DTOs/Auth/LoginRequestDto.cs
+++ Vertical360_backend/Application/DTOs/Auth/LoginRequestDto.cs
@@ -2,9 +2,12 @@
 {
     public class LoginRequestDto
     {
-        public string Document { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public bool RememberMe { get; set; } = false;
+
+        // CompanyId opcional: si se incluye, el servidor generará el token para esa company/tenant
+        public Guid? CompanyId { get; set; }
     }
 }
