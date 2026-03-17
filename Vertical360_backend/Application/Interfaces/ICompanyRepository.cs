184ebdaac54a1483251c602a85012c15e2c7bdc4
Johan Alexis Arango Rueda <johan.arangor@gmail.com>
2026-03-17 00:07:20 -0500

    update applications interfaces companies and tenants

--- Vertical360_backend/Application/Interfaces/ICompanyRepository.cs
+++ Vertical360_backend/Application/Interfaces/ICompanyRepository.cs
@@ -1,4 +1,4 @@
-﻿using Vertical360_backend.Application.DTOs.Companies;
+using Vertical360_backend.Application.DTOs.Companies;
 using Vertical360_backend.Domain.Entities;
 
 namespace Vertical360_backend.Application.Interfaces
@@ -8,5 +8,10 @@ namespace Vertical360_backend.Application.Interfaces
         Task CreateAsync(Companies company);
         Task<List<CompanieResultDto>> GetAllAsync();
         Task<CompanieResultDto?> GetByIdAsync(Guid id);
+        Task<Companies?> GetEntityByIdAsync(Guid id);
+        Task<bool> ExistsByNitAsync(string nit, Guid? excludeId = null);
+        Task UpdateAsync(Companies company);
+        Task<bool> DeleteAsync(Guid id);
     }
 }
+
--- Vertical360_backend/Application/Interfaces/ICompanyService.cs
+++ Vertical360_backend/Application/Interfaces/ICompanyService.cs
@@ -1,10 +1,14 @@
-﻿using Vertical360_backend.Application.DTOs.Companies;
+using Vertical360_backend.Application.DTOs.Companies;
 
 namespace Vertical360_backend.Application.Interfaces
 {
     public interface ICompanyService
     {
-        Task CreateCompanyAsync(CompanyRequestDto dto);
+        Task<CompanieResultDto> CreateCompanyAsync(CompanyRequestDto dto);
         Task<List<CompanieResultDto>> GetAllAsync();
+        Task<CompanieResultDto> GetByIdAsync(Guid id);
+        Task<CompanieResultDto> UpdateAsync(Guid id, CompanyUpdateDto dto);
+        Task<bool> DeleteAsync(Guid id);
     }
 }
+
